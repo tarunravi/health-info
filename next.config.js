8668b257d13f487a386bd7fee681f23902bc2c27
Tarun Ravi <tarunvravi@gmail.com>
2023-12-04 04:20:45 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  rewrites: async () => {
-    return [
-      {
-        source: '/api/:path*',
-        destination:
-          process.env.NODE_ENV === 'development'
-            ? 'http://127.0.0.1:5328/api/:path*'
-            : '/api/',
+module.exports = {
+    async rewrites() {
+        return [
+          {
+            source: '/api/:path*',
+            destination: 'https://api-polbq3pvdq-ue.a.run.app/:path*',
+          },
+        ]
       },
-    ]
-  },
-}
-
-module.exports = nextConfig 
+  };
